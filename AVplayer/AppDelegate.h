2829f1ebc1e09311fe8c5bbe67868b00116f4852
qianfeng <apple@aofel.local>
2014-04-20 09:52:17 +0800

    Initial Commit

---
+++ AVplayer/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  AVplayer
+//
+//  Created by qianfeng on 14-4-20.
+//  Copyright (c) 2014年 aofel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
